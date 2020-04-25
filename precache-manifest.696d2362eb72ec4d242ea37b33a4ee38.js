34f2c98a1cabfaf060af42635d0a49e725f812dc
shailijha <shaili_jha@yahoo.co.in>
2020-04-25 13:23:16 -0500

    Updates

---
+++ precache-manifest.696d2362eb72ec4d242ea37b33a4ee38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353ff95e70b13ad723524d2606cc709e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cc22f1d8dad5a701278",
+    "url": "/static/css/main.aada8eb9.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc22f1d8dad5a701278",
+    "url": "/static/js/main.ee0a4b7f.chunk.js"
+  },
+  {
+    "revision": "ad591d0f4708f88e02d5",
+    "url": "/static/js/runtime-main.ca69820c.js"
+  }
+]);
